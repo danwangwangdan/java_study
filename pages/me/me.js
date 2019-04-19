c38b2f4f9d544fe8c5fc47936e488b14b2aa82d7
hsm <hsm19950709@gmail.com>
2019-04-19 17:14:43 +0800

    Initial commit

---
+++ pages/me/me.js
@@ -0,0 +1,88 @@
+// pages/me/me.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    solveCount: 0,
+    isAdmin: 0,
+    nickname: '',
+    office: '',
+    tel: "",
+    imgSrc: "",
+    isLoading: false,
+    formIds: [],
+    isDisabled: false
+  },
+  toAll: function () {
+    wx.navigateTo({
+      url: '/pages/me/all/all'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    var that = this;
+    var userInfo = wx.getStorageSync("userInfo");
+    var _imgSrc = wx.getStorageSync("imgSrc");
+    if (_imgSrc == "") {
+      _imgSrc = "../../pics/me_photo.png"
+    }
+
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that = this;
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
